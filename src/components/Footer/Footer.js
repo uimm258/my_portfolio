a90a624030e0b41ffdc2ca087810ee34dc3ed137
uimm258 <uimm258@gmail.com>
2021-10-12 14:49:50 -0400

    portfolio

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://arelaliu.github.io'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      @Arela Liu
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,48 +1,37 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://arelaliu.github.io',
+  title: 'Home',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
+  name: 'Arela',
   role: 'Front End Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
-  social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
-  },
+    'Dedicated proficient with 5 years of experience in Web Technologies and user interfaces using HTML5, DOM, CSS3, Bootstrap, JavaScript, React, JQuery, NodeJs, Webpack.',
 }
 
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: 'Yamma',
+    description: 'Provided users with a compilation of up-to-date U.S. news; users can communicate with each other through corresponding article chat windows. Developed API services to receive the up-to-date U.S. news. The API integrated the front-end with the back-end and guaranteed the functionality of the system.',
+    stack: ['React.JS', 'Node.JS', 'HTML', 'CSS', 'PSQL'],
+    sourceCode: 'https://yamma-client-git-development-mettys.vercel.app/'
   },
   {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: 'Spaced Repetition',
+    description:'A self-driven project for tutor use. Developed a website that allows users to learn Chinese words as the quizzing method. Migrated database to Heroku with SQL which allowed the display and translations of words through API calls. Designed and developed the register-and-login system that enables users to save their learning progress.',    
+    stack: ['React.JS', 'Node.JS', 'HTML', 'CSS', 'PSQL'],
+    sourceCode: 'https://spaced-repetition-lime-two.vercel.app/register'
   },
   {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: 'Petful',
+    description: 'Designed and developed a website that allows users to sign up and adopt a pet. Implemented an algorithm with linked lists and sorting for the stored database of dogs and cats to display available pets and to handle the adopting events. Took full ownership of the whole project.',
+    stack: ['React.JS', 'Node.JS', 'HTML', 'CSS', 'PSQL'],
+    sourceCode: 'https://petful-client-app.vercel.app/',
   },
 ]
 
@@ -65,7 +54,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'arela.liu0520@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
